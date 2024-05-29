4d730cfa1fa73a32a7a5b5dc201f827e5a43f288
Mesut GENEZ <mesutgenez@hotmail.com>
2024-05-29 13:21:12 +0300

    input

--- view/page/home_templ.go
+++ view/page/home_templ.go
@@ -12,7 +12,7 @@ import "bytes"
 
 import "github.com/mstgnz/goduty/view"
 
-func Home(name string) templ.Component {
+func Home(message string) templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
 		if !templ_7745c5c3_IsBuffer {
@@ -36,9 +36,9 @@ func Home(name string) templ.Component {
 				return templ_7745c5c3_Err
 			}
 			var templ_7745c5c3_Var3 string
-			templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(name)
+			templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(message)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `view/page/home.templ`, Line: 7, Col: 22}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `view/page/home.templ`, Line: 7, Col: 25}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 			if templ_7745c5c3_Err != nil {
